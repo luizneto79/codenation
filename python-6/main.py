2c836756b6c5b77567f3ba2e2e4a126db742a3f0
Luiz Neto <luizneto79@gmail.com>
2020-05-20 19:59:02 -0300

    Desafio 1 e 2

---
+++ python-6/main.py
@@ -0,0 +1,76 @@
+"""
+Codenation - Acelera DEV - Desafio 2
+Author: Luiz Marques Neto
+Date: 2020-05-20
+"""
+
+
+import abc
+
+
+class Department:
+
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+
+class Employee(metaclass=abc.ABCMeta):
+
+    def __init__(self, code, name, salary):
+        self.code = code
+        self.name = name
+        self.salary = salary
+
+    @abc.abstractmethod
+    def calc_bonus(self):
+        pass
+
+    @abc.abstractmethod
+    def get_hours(self):
+        return 8
+
+
+class Manager(Employee):
+
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary)
+        self.__departament = Department('managers', 1)
+
+    def calc_bonus(self):
+        return self.salary * 0.15
+
+    def get_departament(self):
+        return self.__departament.name
+
+    def set_departament(self, description):
+        self.__departament = description.name
+
+    def get_hours(self):
+        return super().get_hours()
+
+
+class Seller(Employee):
+
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary)
+        self.__departament = Department('sellers', 2)
+        self.__sales = 0
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sales):
+        self.__sales += sales
+
+    def get_hours(self):
+        return super().get_hours()
+
+    def get_departament(self):
+        return self.__departament.name
+
+    def set_departament(self, description):
+        self.__departament = description
