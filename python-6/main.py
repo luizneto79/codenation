e1c0eca8f0daab063ef276ceb2c4242170e093a8
Luiz Marques Neto <luizneto79@gmail.com>
2020-05-20 20:51:20 -0300

    Update main.py

--- python-6/main.py
+++ python-6/main.py
@@ -14,13 +14,21 @@ class Department:
         self.name = name
         self.code = code
 
+    def __str__(self):
+        return self.code + ' ' + self.name
+
 
 class Employee(metaclass=abc.ABCMeta):
 
-    def __init__(self, code, name, salary):
+    def __init__(self, code, name, salary, department):
         self.code = code
         self.name = name
         self.salary = salary
+        self.__WORK_HOUR = 8
+        self.__departament = department
+
+    def __str__(self):
+        return self.name + ' ' + self.get_departament()
 
     @abc.abstractmethod
     def calc_bonus(self):
@@ -28,23 +36,22 @@ class Employee(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def get_hours(self):
-        return 8
-
-
-class Manager(Employee):
-
-    def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('managers', 1)
-
-    def calc_bonus(self):
-        return self.salary * 0.15
+        return self.__WORK_HOUR
 
     def get_departament(self):
         return self.__departament.name
 
     def set_departament(self, description):
-        self.__departament = description.name
+        self.__departament = description
+
+
+class Manager(Employee):
+
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary, Department('managers', 1))
+
+    def calc_bonus(self):
+        return self.salary * 0.15
 
     def get_hours(self):
         return super().get_hours()
@@ -53,8 +60,7 @@ class Manager(Employee):
 class Seller(Employee):
 
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('sellers', 2)
+        super().__init__(code, name, salary, Department('sellers', 2))
         self.__sales = 0
 
     def calc_bonus(self):
@@ -69,8 +75,3 @@ class Seller(Employee):
     def get_hours(self):
         return super().get_hours()
 
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_departament(self, description):
-        self.__departament = description
